1d29c1ef625104be262f60664247a06acaea2c1e
Travis C <140869835+TravMan3@users.noreply.github.com>
2023-10-24 09:35:59 -0600

    UPDATE 10.24.23

--- src/com/coderscampus/UserLoginApplication.java
+++ src/com/coderscampus/UserLoginApplication.java
@@ -1,10 +1,30 @@
 package com.coderscampus;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class UserLoginApplication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		Scanner scanner = new Scanner(System.in);			
+		UserService userService = new UserService();
+		User user = new User();
+		
+//		String username = userService.input(scanner,"\n" + "Enter your email: " + "\n");
+//		String password = userService.input(scanner,"\n" + "Enter your password: " + "\n");
+		String data = userService.getData();
+		String[] array = userService.parseText(data);
+		System.out.println(Arrays.toString(array));
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		System.out.println(array[2]);
+		System.out.println(array[3]);
+		System.out.println(user.getUsername());
+		user.setUsername(array[1]);
+		System.out.println(user.getUsername());
+		scanner.close();
 	}
-
+	
 }
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -1,5 +1,40 @@
 package com.coderscampus;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class UserService {
+
+	public String input(Scanner scanner, String displayedMessage) {
+		System.out.println(displayedMessage);
+		String userInput = scanner.nextLine();
+		return userInput;
+	}
+
+	public String getData() throws IOException {
+		StringBuilder result = new StringBuilder();
+		BufferedReader fileReader = null;
+		String line;
+		try {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+
+			while ((line = fileReader.readLine()) != null) {
+				result.append(line + "\n");
+
+			}
+
+		} finally {
+			fileReader.close();
+		}
+		return result.toString();
+	}
+
+	public String[] parseText(String input) {
+		return input.split(",");
+	}
+	
+//	public String 
 
 }
